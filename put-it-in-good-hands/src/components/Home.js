d807c6cb461f493d42101d674566bb77f014a6a3
Wojciech Kiryk <wojciech.kiryk@gmail.com>
2020-07-01 15:08:53 +0200

    basic config.

---
+++ put-it-in-good-hands/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import HomeHeader from './Home/welcome_section/HomeNav';
+import Contact from './Home/ending_section/ContactForm';
+
+export default () => (
+    <>
+        <h1>To jest Home</h1>
+        <HomeHeader />
+        <Contact />
+    </>
+)
--- put-it-in-good-hands/src/components/Home/welcome_section/HomeNav.js
+++ put-it-in-good-hands/src/components/Home/welcome_section/HomeNav.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default () => (
-    <h1>To jest Header</h1>
+    <nav>To jest nawigacja</nav>
 )
