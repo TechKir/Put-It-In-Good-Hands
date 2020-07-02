84daaab4dc848c1aa84f8102ddf54ba52f103026
Wojciech Kiryk <wojciech.kiryk@gmail.com>
2020-07-02 22:39:59 +0200

    mobile -navigation

--- put-it-in-good-hands/src/components/Home.js
+++ put-it-in-good-hands/src/components/Home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import HomeHeader from './Home/welcome_section/HomeNav';
-import Contact from './Home/ending_section/ContactForm';
+import HomeNav from './Home/welcome_section/HomeNav';
+//import Contact from './Home/ending_section/ContactForm';
 
 export default () => (
     <>
-        <h1>To jest Home</h1>
-        <HomeHeader />
-        <Contact />
+        <HomeNav/>
     </>
 )
--- put-it-in-good-hands/src/components/Home/welcome_section/HomeNav.js
+++ put-it-in-good-hands/src/components/Home/welcome_section/HomeNav.js
@@ -1,5 +1,33 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import classNames from 'classnames';
 
-export default () => (
-    <nav>To jest nawigacja</nav>
-)
+export default () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isActive, setIsActive]=useState(false)
+
+
+    const showNav = () =>{
+        setIsOpen(prevState => !prevState)
+        setIsActive(prevState => !prevState)
+    };
+
+    return (
+        <>
+            <nav className={classNames('navigation',{hamburgerActive: isActive})}>
+                <div className='homeImg'></div>
+                <button className='hamburger' onClick={showNav}>
+                    <span className='hamburgerBox'>
+                        <span className='hamburgerInner'></span>
+                    </span>
+                </button>
+            </nav>
+            <ul className={classNames('navMenu', {hiddenMenu: isOpen})}>
+                <li className="navOptions">Start</li>
+                <li className="navOptions">O co chodzi?</li>
+                <li className="navOptions">O nas</li>
+                <li className="navOptions">Fundacja i organizacje</li>
+                <li className="navOptions">Kontakt</li>
+            </ul> 
+        </>
+    )
+}
