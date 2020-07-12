a72326262301d837f1fdcb7c8ea62c8aa8b625b8
Wojciech Kiryk <wojciech.kiryk@gmail.com>
2020-07-12 23:27:11 +0200

    ready login logout register layouts

---
+++ put-it-in-good-hands/src/components/LogOut.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import decoration from '../assets/Decoration.svg'
+import HomeNav from './Home/welcome_section/HomeNav'
+import {
+    HashRouter,
+    Route,
+    Link,
+    Switch,
+    NavLink,
+    } from 'react-router-dom';
+
+export default () => {
+
+    return(
+        <>
+            <HomeNav/>
+            <h2 className='textUpDecoration' id='section4'>
+                Wylogowanie nastąpiło <br/> pomyślnie
+            </h2>
+            <div className='decorationBox'>
+                <img src={decoration} alt='decoration'/>
+            </div>
+            <div className='btnBox'>
+                <button className='mainPageBtn'>
+                    Strona główna
+                </button>
+            </div>
+
+        </>
+    )
+}
